deee9409eca23c028f97637cfc189e00a550c01d
Nabin Rai <rainabinkhyal@gmail.com>
2023-08-07 15:50:57 +0545

    KeyPair generation

--- src/main/java/com/khaling/springsecurityjwt2/configuration/SecurityConfiguration.java
+++ src/main/java/com/khaling/springsecurityjwt2/configuration/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -34,7 +35,7 @@ public class SecurityConfiguration {
                     auth.requestMatchers("/auth/**").permitAll();
                     auth.anyRequest().authenticated();
                 })
-                .httpBasic().and()
+                .httpBasic(Customizer.withDefaults())
                 .build();
     }
 
